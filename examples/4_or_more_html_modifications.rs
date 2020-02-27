5d7496addd4b189687400dd8c73d42b87022fdea
Anton Lazarev <antonok35@gmail.com>
2020-02-27 13:42:46 -0500

    support querying HTML element modifications

---
+++ examples/4_or_more_html_modifications.rs
@@ -0,0 +1,38 @@
+//! Prints out all HTML elements from a page that were modified at least 4 times, then prints out
+//! those nodes and their respective modifications sorted by timestamp.
+
+use pagegraph::from_xml::read_from_file;
+use pagegraph::types::NodeType;
+
+fn main() {
+    let graph_file = std::env::args().skip(1).next().expect("Provide a path to a `.graphml` file");
+    let graph = read_from_file(&graph_file);
+
+    let html_elements = graph.filter_nodes(|node_type| {
+        match node_type {
+            NodeType::HtmlElement { .. } => true,
+            _ => false,
+        }
+    });
+
+    let mut heavily_modified_elements: Vec<_> = html_elements.iter().filter_map(|(node_id, _node)| {
+        let num_modifications = graph.all_html_element_modifications(**node_id).len();
+        if num_modifications >= 4 {
+            Some((*node_id, num_modifications))
+        } else {
+            None
+        }
+    }).collect();
+
+    heavily_modified_elements.sort_by(|(_, a), (_, b)| b.cmp(a));
+
+    heavily_modified_elements.iter().rev().for_each(|(id, num)| {
+        println!("{:?} was modified {} times", id, num);
+    });
+
+    heavily_modified_elements.iter().map(|(id, _)| *id).for_each(|id| {
+        let modifications = graph.all_html_element_modifications(*id);
+        dbg!(graph.nodes.get(id));
+        dbg!(modifications);
+    });
+}
--- src/graph_algos.rs
+++ src/graph_algos.rs
@@ -1,6 +1,8 @@
 use crate::graph::{ PageGraph, Edge, EdgeId, Node, NodeId };
 use crate::types::{ EdgeType, NodeType };
 
+use petgraph::Direction;
+
 impl PageGraph {
     pub fn filter_edges<F: Fn(&EdgeType) -> bool>(&self, f: F) -> Vec<(&EdgeId, &Edge)> {
         self.edges.iter().filter(|(_id, edge)| {
@@ -12,5 +14,29 @@ impl PageGraph {
         self.nodes.iter().filter(|(_id, node)| {
             f(&node.node_type)
         }).collect()
+    }
+
+    /// Returns a sorted Vec including 1 edge representing every time the given HtmlElement node was
+    /// modified in the page.
+    pub fn all_html_element_modifications(&self, node_id: NodeId) -> Vec<(&EdgeId, &Edge)> {
+        let element = self.nodes.get(&node_id).unwrap();
+
+        if let NodeType::HtmlElement { node_id: _html_node_id, .. } = element.node_type {
+            let mut modifications: Vec<_> = self.graph.neighbors_directed(node_id, Direction::Incoming).map(|node| {
+                let edge_id = self.graph.edge_weight(node, node_id).unwrap();
+                (edge_id, self.edges.get(edge_id).unwrap())
+            }).filter(|(_id, edge)| {
+                match edge.edge_type {
+                    EdgeType::Structure { .. } => false,
+                    _ => true,
+                }
+            }).collect();
+
+            modifications.sort_by_key(|(_, edge)| edge.edge_timestamp.expect("HTML element modification had no timestamp"));
+
+            modifications
+        } else {
+            panic!("Supply a node with HtmlElement node type");
+        }
     }
 }
