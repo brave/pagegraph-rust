3b011b07be7bfdf9fa52e0280a8fce28283a9906
Anton Lazarev <antonok35@gmail.com>
2020-12-14 10:38:39 -0500

    add additional iterator-focused convenience methods for graphs

--- src/graph.rs
+++ src/graph.rs
@@ -49,6 +49,46 @@ impl PageGraph {
         assert!(!self.edges.contains_key(&new_id));
         new_id
     }
+
+    pub fn source_node<'a>(&'a self, edge: &Edge) -> &'a Node {
+        self.nodes.get(&edge.source).unwrap_or_else(|| panic!("Source node for edge {:?} could not be found in the graph", edge))
+    }
+
+    pub fn target_node<'a>(&'a self, edge: &Edge) -> &'a Node {
+        self.nodes.get(&edge.target).unwrap_or_else(|| panic!("Target node for edge {:?} could not be found in the graph", edge))
+    }
+
+    pub fn outgoing_edges<'a>(&'a self, node: &Node) -> impl Iterator<Item=&'a Edge> {
+        self.edges_iter_directed(node, petgraph::Direction::Outgoing)
+    }
+
+    pub fn incoming_edges<'a>(&'a self, node: &Node) -> impl Iterator<Item=&'a Edge> {
+        self.edges_iter_directed(node, petgraph::Direction::Incoming)
+    }
+
+    fn edges_iter_directed<'a>(&'a self, node: &Node, direction: petgraph::Direction) -> impl Iterator<Item=&'a Edge> {
+        self.graph.edges_directed(node.id, direction).map(move |(_a, _b, edge_ids)| {
+            edge_ids
+        })
+            .flatten()
+            .map(move |edge_id| {
+                self.edges.get(&edge_id).unwrap()
+            })
+    }
+
+    pub fn outgoing_neighbors<'a>(&'a self, node: &Node) -> impl Iterator<Item=&'a Node> {
+        self.nodes_iter_directed(node, petgraph::Direction::Outgoing)
+    }
+
+    pub fn incoming_neighbors<'a>(&'a self, node: &Node) -> impl Iterator<Item=&'a Node> {
+        self.nodes_iter_directed(node, petgraph::Direction::Incoming)
+    }
+
+    fn nodes_iter_directed<'a>(&'a self, node: &Node, direction: petgraph::Direction) -> impl Iterator<Item=&'a Node> {
+        self.graph.neighbors_directed(node.id, direction).map(move |node_id| {
+            self.nodes.get(&node_id).unwrap()
+        })
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, PartialOrd, Ord)]
